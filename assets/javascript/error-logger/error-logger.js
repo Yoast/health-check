25570917feca9156b7cbf3e696175bc499d3ed56
Omar Reiss <omar.reiss@gmail.com>
2020-11-16 20:46:07 +0100

    Cleanup error logger

--- assets/javascript/error-logger/error-logger.js
+++ assets/javascript/error-logger/error-logger.js
@@ -1,4 +1,17 @@
+/* global console XMLHttpRequest */
+/**
+ * Subscribes to all JS errors and logs them to the server when they occur.
+ *
+ * @return {void}
+ */
 ( function() {
+	/**
+	 * Posts a JS error via WP Ajax to Site Health.
+	 *
+	 * @param {Object} error The error to log.
+	 *
+	 * @return {void}
+	 */
 	function postError( error ) {
 		const http = new XMLHttpRequest();
 		const params = 'action=health-check-log-errors' +
@@ -7,19 +20,31 @@
 		http.open( 'POST', window.ajaxurl, true );
 		http.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded;' );
 		http.onreadystatechange = function() {
-			console.warn( 'Error successfully logged to', window.SiteHealthErrorLogger.logURL, error );
+			if ( http.readyState === XMLHttpRequest.DONE ) {
+				// eslint-disable-next-line no-console
+				console.warn( 'Error successfully logged to', window.SiteHealthErrorLogger.logURL, error );
+			}
 		};
 		http.send( params );
 	}
 
 	let errorMessage = '';
 
+	/**
+	 * Callback function for the onerror event. Logs errors to the server.
+	 *
+	 * @param {string} msg The error message.
+	 * @param {string} url The url of the file where the error occurred.
+	 * @param {number} lineNo The line number where the error occurred.
+	 * @param {number} columnNo The column number where the error occurred.
+	 * @param {ErrorEvent} err The error event.
+	 * @return {boolean} False.
+	 */
 	window.onerror = function( msg, url, lineNo, columnNo, err ) {
 		let error = {};
 		const string = msg.toLowerCase();
 		const substring = 'script error';
 		if ( string.indexOf( substring ) > -1 ) {
-			console.log( msg, url, lineNo, columnNo, err );
 			error = {
 				message: 'External Script error',
 				url: 'external',
